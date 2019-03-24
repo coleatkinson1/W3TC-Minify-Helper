5330c14e43f45befb0ca19a839798f71a01dc265
coleatkinson1 <40928853+coleatkinson1@users.noreply.github.com>
2019-03-24 17:53:18 +1300

    setup plugin

---
+++ W3TC-Minify-Helper.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: W3TC Minify Helper
+ * Plugin URI: https://github.com/coleatkinson1/W3TC-Minify-Helper
+ * Description: Detects and loads CSS/JS scripts that require minification and render-blocking setup into W3TC
+ * Version: 1.0
+ * Author: Cole Atkinson
+ * Author URI: http://coleatkinson.nz
+ */
+
+ 
+ ?>
